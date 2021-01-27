be6effe0661993fef80519bb070b2987dd1f9be9
Nicole Cox <nicoleanalisecox@gmail.com>
2021-01-27 21:18:24 +0000

    add ReadFile method that reads in text file (.asm) with a defined path, add WriteFile method that writes a test output text file (.hack)

--- N2T-Assembler/Program.cs
+++ N2T-Assembler/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace N2T_Assembler
 {
@@ -6,7 +7,62 @@ namespace N2T_Assembler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            ReadFile();
+            WriteFile();
+        }
+
+        static void ReadFile()
+        {
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\test\\Fill.asm");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        static void WriteFile()
+        {
+            try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("C:\\test\\Test.hack", false);
+                //Write a line of text
+                sw.WriteLine("This is a test");
+                //Write a second line of text
+                sw.WriteLine("using my WriteFile function");
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
         }
 
     }
