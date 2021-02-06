82bea575e68f078dc00f13f249913d781f169d98
Nicole Cox <nicoleanalisecox@gmail.com>
2021-02-06 23:52:48 +0000

    implement code to accomodate symbols (labels & variables) - successfully assembles Add, Max, Rect & Pong assembly files to machine code!

--- N2T-Assembler/Program.cs
+++ N2T-Assembler/Program.cs
@@ -2,166 +2,242 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 
 namespace N2T_Assembler
 {
-    internal class Assembler
+    public class Assembler
     {
+        public static string CleanData(string dataIn)
+        {
+            return dataIn.Split(new[] { "//" }, StringSplitOptions.None)[0].Trim().Replace(" ", string.Empty);
+        }
+
         private static void Main(string[] args)
 
         {
             //file input & naming
-            string sourceFilePath = "C:\\test\\PongL.asm";
-            string fileNameIn = Path.GetFileName(sourceFilePath);
+            string sourceFilePath = "C:\\test\\Pong.asm";
             string fileNameOut = Path.GetFileNameWithoutExtension(sourceFilePath) + ".hack";
             string destFilePath = Path.GetDirectoryName(sourceFilePath) + "\\" + fileNameOut;
 
             //read file
             IEnumerable<string> lines = File.ReadLines(sourceFilePath);
 
-            // comp bits dictionary
-            Dictionary<string, string> compDictionary =
-                new Dictionary<string, string>();
-
-            compDictionary.Add("0", "101010");
-            compDictionary.Add("1", "111111");
-            compDictionary.Add("-1", "111010");
-            compDictionary.Add("D", "001100");
-            compDictionary.Add("A", "110000");
-            compDictionary.Add("M", "110000");
-            compDictionary.Add("!D", "001101");
-            compDictionary.Add("!A", "110001");
-            compDictionary.Add("!M", "110001");
-            compDictionary.Add("-D", "001111");
-            compDictionary.Add("-A", "110011");
-            compDictionary.Add("-M", "110011");
-            compDictionary.Add("D+1", "011111");
-            compDictionary.Add("A+1", "110111");
-            compDictionary.Add("M+1", "110111");
-            compDictionary.Add("D-1", "001110");
-            compDictionary.Add("A-1", "110010");
-            compDictionary.Add("M-1", "110010");
-            compDictionary.Add("D+A", "000010");
-            compDictionary.Add("D+M", "000010");
-            compDictionary.Add("D-A", "010011");
-            compDictionary.Add("D-M", "010011");
-            compDictionary.Add("A-D", "000111");
-            compDictionary.Add("M-D", "000111");
-            compDictionary.Add("D&A", "000000");
-            compDictionary.Add("D&M", "000000");
-            compDictionary.Add("D|A", "010101");
-            compDictionary.Add("D|M", "010101");
-
-            // destination bits dictionary
-            Dictionary<string, string> destDictionary =
-                new Dictionary<string, string>();
-
-            destDictionary.Add("null", "000");
-            destDictionary.Add("M", "001");
-            destDictionary.Add("D", "010");
-            destDictionary.Add("MD", "011");
-            destDictionary.Add("A", "100");
-            destDictionary.Add("AM", "101");
-            destDictionary.Add("AD", "110");
-            destDictionary.Add("AMD", "111");
-
-            // jump bits dictionary
-            Dictionary<string, string> jumpDictionary =
-                new Dictionary<string, string>();
-
-            jumpDictionary.Add("null","000");
-            jumpDictionary.Add("JGT", "001");
-            jumpDictionary.Add("JEQ", "010");
-            jumpDictionary.Add("JGE", "011");
-            jumpDictionary.Add("JLT", "100");
-            jumpDictionary.Add("JNE", "101");
-            jumpDictionary.Add("JLE", "110");
-            jumpDictionary.Add("JMP", "111");
-
+            //initialise symbol table
             // dictionary for pre-defined symbols
-            Dictionary<string, Int32> symbolDictionary =
-                new Dictionary<string, Int32>();
-
-            compDictionary.Add("R0", "0");
-            compDictionary.Add("R1", "1");
-            compDictionary.Add("R2", "2");
-            compDictionary.Add("R3", "3");
-            compDictionary.Add("R4", "4");
-            compDictionary.Add("R5", "5");
-            compDictionary.Add("R6", "6");
-            compDictionary.Add("R7", "7");
-            compDictionary.Add("R8", "8");
-            compDictionary.Add("R9", "9");
-            compDictionary.Add("R10", "10");
-            compDictionary.Add("R11", "11");
-            compDictionary.Add("R12", "12");
-            compDictionary.Add("R13", "13");
-            compDictionary.Add("R14", "14");
-            compDictionary.Add("R15", "15");
-            compDictionary.Add("SCREEN", "16384");
-            compDictionary.Add("KBD", "24576");
-
-            // write to file
-            string[] dataToWrite = lines.ToArray();
-            string convertedString = "";
-            string comp = "";
-
-            using (StreamWriter outputFile = new StreamWriter(destFilePath))
-            {
-                foreach (string line in dataToWrite)
+            Dictionary<string, int> symbolDictionary =
+                new Dictionary<string, int>
                 {
-                    if (line.StartsWith("//") || string.IsNullOrWhiteSpace(line))
+                    {"R0", 0},
+                    {"R1", 1},
+                    {"R2", 2},
+                    {"R3", 3},
+                    {"R4", 4},
+                    {"R5", 5},
+                    {"R6", 6},
+                    {"R7", 7},
+                    {"R8", 8},
+                    {"R9", 9},
+                    {"R10", 10},
+                    {"R11", 11},
+                    {"R12", 12},
+                    {"R13", 13},
+                    {"R14", 14},
+                    {"R15", 15},
+                    {"SCREEN", 16384},
+                    {"KBD", 24576},
+                    {"SP", 0},
+                    {"LCL", 1},
+                    {"ARG", 2},
+                    {"THIS", 3},
+                    {"THAT", 4}
+                };
+
+            // first pass - look for labels only
+            string[] linesToRead = lines.ToArray();
+            var linesRead = 0;
+
+            foreach (string lineToRead in linesToRead)
+            {
+                if (lineToRead.StartsWith("//") || string.IsNullOrWhiteSpace(lineToRead))
+                {
+                    continue;
+                }
+
+                if (lineToRead.StartsWith("("))
+                {
+                    var label = lineToRead.Split('(', ')')[1];
+                    symbolDictionary.Add(label, linesRead);
+                }
+                else
+                {
+                    linesRead++;
+                }
+            }
+
+            Console.WriteLine("FIRST PASS COMPLETE");
+            var assignedAddress = 16;
+
+            //second pass - look for variables
+            foreach (string lineToRead in linesToRead)
+            {
+                if (lineToRead.StartsWith("//") || string.IsNullOrWhiteSpace(lineToRead))
+                {
+                    continue;
+                }
+
+                if (lineToRead.Contains("@"))
+                {
+                    var customSymbol = lineToRead.Split('@')[1];
+                    bool isNumeric = int.TryParse(customSymbol, out _);
+                    bool symbolExists = symbolDictionary.ContainsKey(customSymbol);
+
+                    if (!symbolExists && !isNumeric)
                     {
-                        continue;
+                        symbolDictionary.Add(customSymbol, assignedAddress);
+                        assignedAddress++;
                     }
-                    else  
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            Console.WriteLine("SECOND PASS COMPLETE");
+            foreach (KeyValuePair<string, int> kvp in symbolDictionary)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",
+                    kvp.Key, kvp.Value);
+            }
+
+            foreach (string lineToRead in linesToRead)
+            {
+                //third pass - check for A or C instruction, translate to binary & write to file
+                // comp bits dictionary
+                Dictionary<string, string> compDictionary =
+                    new Dictionary<string, string>
                     {
-                        
-                        // break it down now y'all
-                        if (line.StartsWith("@"))
+                        {"0", "101010"},
+                        {"1", "111111"},
+                        {"-1", "111010"},
+                        {"D", "001100"},
+                        {"A", "110000"},
+                        {"M", "110000"},
+                        {"!D", "001101"},
+                        {"!A", "110001"},
+                        {"!M", "110001"},
+                        {"-D", "001111"},
+                        {"-A", "110011"},
+                        {"-M", "110011"},
+                        {"D+1", "011111"},
+                        {"A+1", "110111"},
+                        {"M+1", "110111"},
+                        {"D-1", "001110"},
+                        {"A-1", "110010"},
+                        {"M-1", "110010"},
+                        {"D+A", "000010"},
+                        {"D+M", "000010"},
+                        {"D-A", "010011"},
+                        {"D-M", "010011"},
+                        {"A-D", "000111"},
+                        {"M-D", "000111"},
+                        {"D&A", "000000"},
+                        {"D&M", "000000"},
+                        {"D|A", "010101"},
+                        {"D|M", "010101"}
+                    };
+
+                // destination bits dictionary
+                Dictionary<string, string> destDictionary =
+                    new Dictionary<string, string>
+                    {
+                        {"null", "000"},
+                        {"M", "001"},
+                        {"D", "010"},
+                        {"MD", "011"},
+                        {"A", "100"},
+                        {"AM", "101"},
+                        {"AD", "110"},
+                        {"AMD", "111"}
+                    };
+
+                // jump bits dictionary
+                Dictionary<string, string> jumpDictionary =
+                    new Dictionary<string, string>
+                    {
+                        {"null", "000"},
+                        {"JGT", "001"},
+                        {"JEQ", "010"},
+                        {"JGE", "011"},
+                        {"JLT", "100"},
+                        {"JNE", "101"},
+                        {"JLE", "110"},
+                        {"JMP", "111"}
+                    };
+
+                // write to file
+                var dataToWrite = lines.ToArray();
+
+                using (StreamWriter outputFile = new StreamWriter(destFilePath))
+                {
+                    foreach (var line in dataToWrite)
+                    {
+                        if (line.StartsWith("//") || string.IsNullOrWhiteSpace(line))
                         {
-                            //process A instruction
-                            string address = line.Split('@')[1];
-                            var convertedAddress = Convert.ToString(Int32.Parse(address), 2).PadLeft(16,'0');
-                            outputFile.WriteLine(convertedAddress);
+                            continue;
                         }
-
-                        if (line.Contains("=") || line.Contains(";"))
+                        else
                         {
-                            
-                            //process C instruction
-
-                            if (line.Contains("=") && !line.Contains(";")) //e.g. MD=M+1
+                            var cleanLine = CleanData(line);
+                            // break it down now y'all
+                            if (cleanLine.StartsWith("@"))
                             {
-                                
-                                var dest = line.Split('=')[0];
-                                string destConverted = destDictionary[dest];
+                                //process A instruction
+                                var addressPart = cleanLine.Split('@')[1];
 
-                                comp = line.Split('=', ';')[1];
-                                string compConverted = compDictionary[comp];
-                                var aBit = comp.Contains("M") ? "1" : "0";
+                                var address = symbolDictionary.ContainsKey(addressPart)
+                                    ? Convert.ToString(symbolDictionary[addressPart])
+                                    : addressPart;
 
-                                convertedString = compConverted + destConverted + "000";
-                                outputFile.WriteLine("111" + aBit + convertedString);
+                                var convertedAddress = Convert.ToString(int.Parse(address), 2).PadLeft(16, '0');
+                                outputFile.WriteLine(convertedAddress);
                             }
 
-                            if (!line.Contains("=") && line.Contains(";")) //e.g. 0;JMP
+                            if (cleanLine.Contains("=") || cleanLine.Contains(";"))
                             {
-                                comp = line.Split('=', ';')[0];
-                                string compConverted = compDictionary[comp];
-                                var aBit = comp.Contains("M") ? "1" : "0";
+                                //process C instruction
+                                string convertedString;
+                                string comp;
+                                if (cleanLine.Contains("=") && !cleanLine.Contains(";")) //e.g. MD=M+1
+                                {
+                                    var dest = cleanLine.Split('=')[0];
+                                    var destConverted = destDictionary[dest];
 
-                                var jump = line.Split('=', ';')[1];
-                                string jumpConverted = jumpDictionary[jump];
+                                    comp = cleanLine.Split('=', ';')[1];
+                                    var compConverted = compDictionary[comp];
+                                    var aBit = comp.Contains("M") ? "1" : "0";
 
-                                convertedString = compConverted + "000" + jumpConverted;
-                                outputFile.WriteLine("111" + aBit + convertedString);
+                                    convertedString = compConverted + destConverted + "000";
+                                    outputFile.WriteLine("111" + aBit + convertedString);
+                                }
 
-                            }
-                            else
-                            {
-                                continue;
+                                if (!cleanLine.Contains("=") && cleanLine.Contains(";")) //e.g. 0;JMP
+                                {
+                                    comp = cleanLine.Split('=', ';')[0];
+                                    var compConverted = compDictionary[comp];
+                                    var aBit = comp.Contains("M") ? "1" : "0";
+
+                                    var jump = cleanLine.Split('=', ';')[1];
+                                    var jumpConverted = jumpDictionary[jump];
+
+                                    convertedString = compConverted + "000" + jumpConverted;
+                                    outputFile.WriteLine("111" + aBit + convertedString);
+                                }
+                                else
+                                {
+                                    continue;
+                                }
                             }
                         }
 
@@ -169,7 +245,7 @@ namespace N2T_Assembler
                 }
             }
 
-            Console.WriteLine("Translation complete.");
+            Console.WriteLine("File successfully assembled.");
         }
     }
 }
